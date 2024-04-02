aded6894847f319dd0eb5c92848a5b37d0f5c0f5
rafalzietak <rzi@vp.pl>
2024-04-02 21:44:26 +0200

    send sms after 2s

--- app/src/main/java/com/example/sms3/SecondActivity.java
+++ app/src/main/java/com/example/sms3/SecondActivity.java
@@ -6,10 +6,14 @@ import static com.example.sms3.MainActivity.isOn;
 import static com.example.sms3.MainActivity.mylist;
 import static com.example.sms3.MainActivity.tvQty;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -17,7 +21,7 @@ import android.widget.Toast;
 import java.util.Iterator;
 
 public class SecondActivity extends AppCompatActivity {
-    public String mNumber, mText, mState;
+    public String mNumber, mText, mState, number,state;
     public String message ="ha ha ha ";
 
     @Override
@@ -26,8 +30,8 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         // getIntent() is a method from the started activity
         Intent myIntent = getIntent(); // gets the previously created intent
-        String number = myIntent.getStringExtra("number"); // will return "FirstKeyValue"
-        String state = myIntent.getStringExtra("state");
+        number = myIntent.getStringExtra("number"); // will return "FirstKeyValue"
+        state = myIntent.getStringExtra("state");
         Log.d("msg", "number from Intent= " +number + " state= "+ state);
         message ="ha ha ha ";
 
@@ -48,25 +52,10 @@ public class SecondActivity extends AppCompatActivity {
                     Log.d("msg" , "mNumber = "+ mNumber + " mText = " + mText);
                     //Toast.makeText(getApplicationContext(), "Wysłanie SMS na numer telefonu  "+ number, Toast.LENGTH_LONG).show();
                     if (isOn && mylist.size() >0 && cbWhenDecline.isChecked()) {
-                        try {
-                            // Get the default instance of the SmsManager
-                            SmsManager smsManager = SmsManager.getDefault();
-                            smsManager.sendTextMessage(number,
-                                    null,
-                                    mText,
-                                    null,
-                                    null);
-                            Log.d("msg", "sms sent");
-                            Toast.makeText(getApplicationContext(), "Wysano SMS do dzwoniącego o treści: \n " + mText, Toast.LENGTH_LONG).show();
-                            count= count+1;
-                            Log.d("msg", "count = " + count);
-                            tvQty.setText(String.valueOf(count));
+                        Log.d("msg" , "send sms");
 
-                        } catch (Exception ex) {
-                            Log.d("msg", "problem with sending of sms");
-                            Toast.makeText(getApplicationContext(), "Błąd wysyłania SMS! ", Toast.LENGTH_LONG).show();
-                            ex.printStackTrace();
-                        }
+                        sendSms();
+
                     }else {
                         Log.d("msg", "Nie wysano SMS ponieważ nie włączone");
                         Toast.makeText(getApplicationContext(), "Nie wysłano SMS ponieważ nie włączone", Toast.LENGTH_LONG).show();
@@ -75,5 +64,35 @@ public class SecondActivity extends AppCompatActivity {
             }
         }
         finish();
+    }
+
+    private void sendSms() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Your Code
+                try {
+                    // Get the default instance of the SmsManager
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(number,
+                            null,
+                            mText,
+                            null,
+                            null);
+                    Log.d("msg", "sms sent");
+                    Toast.makeText(getApplicationContext(), "Wysano SMS do dzwoniącego o treści: \n " + mText, Toast.LENGTH_LONG).show();
+                    count= count+1;
+                    Log.d("msg", "count = " + count);
+                    tvQty.setText(String.valueOf(count));
+
+                } catch (Exception ex) {
+                    Log.d("msg", "problem with sending of sms");
+                    Toast.makeText(getApplicationContext(), "Błąd wysyłania SMS! ", Toast.LENGTH_LONG).show();
+                    ex.printStackTrace();
+                }
+            }
+        }, 2000);
+
+
     }
 }
